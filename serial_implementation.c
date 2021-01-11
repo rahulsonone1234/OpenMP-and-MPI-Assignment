60ba40c651403d785fb68bcfcb238f736074928e
Rahul Sonone <rahulsonone1234@gmail.com>
2021-01-12 05:19:31 +0530

    Update serial_implementation.c

--- serial_implementation.c
+++ serial_implementation.c
@@ -1,40 +1,4 @@
 #include<stdio.h>
-
-int main()
-{
-    int n,m;
-    scanf("%d %d",&n,&m);
-    int a[n][m];
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<m;j++)
-        {
-            scanf("%d",&a[i][j]);
-        }
-    }
-    int s=44;
-    int s1=s/10;
-    int s2=s%10;
-
-    int i, j, thread_id, cnt;
-	cnt=0; 
-	    for(i=0;i<n;i++)
-	    {
-		for(j=0;j<m;j++)
-		{
-		    if(a[i][j]==s1)
-		    {
-		        if(a[i][j+1]==s2)
-		        {
-		            cnt++;
-		            a[i][j]=0;
-		            a[i][j+1]=0;
-		        }
-		        if(a[i+1][j+1]==s2)
-		        {
-		            cnt++;
-		            a[i][j]=0;
-#include<stdio.h>
 #include<omp.h>
 
 int main()
