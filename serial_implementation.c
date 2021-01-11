54d54d3b5e80d85dc7bd79847c937c117a3050fd
Rahul Sonone <rahulsonone1234@gmail.com>
2021-01-11 22:36:39 +0530

    Update serial_implementation.c

--- serial_implementation.c
+++ serial_implementation.c
@@ -34,6 +34,44 @@ int main()
 		        {
 		            cnt++;
 		            a[i][j]=0;
+#include<stdio.h>
+#include<omp.h>
+
+int main()
+{
+    int n,m;
+    scanf("%d %d",&n,&m);
+    int a[n][m];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    int s=44;
+    int s1=s/10;
+    int s2=s%10;
+
+    int i, j, thread_id, cnt;
+	cnt=0; 
+	double startTime = omp_get_wtime();
+	    for(i=0;i<n;i++)
+	    {
+		for(j=0;j<m;j++)
+		{
+		    if(a[i][j]==s1)
+		    {
+		        if(a[i][j+1]==s2)
+		        {
+		            cnt++;
+		            a[i][j]=0;
+		            a[i][j+1]=0;
+		        }
+		        if(a[i+1][j+1]==s2)
+		        {
+		            cnt++;
+		            a[i][j]=0;
 		            a[i+1][j+1]=0;
 		        }
 		        if(a[i+1][j]==s2)
@@ -51,9 +89,10 @@ int main()
 		    }
 		}
 	    }
+ double endTime = omp_get_wtime();
+  printf("%lf",endTime - startTime);
 
-
-    printf("%d ", cnt);
+    printf("\n %d ", cnt);
     return 0;
 }
 /*
